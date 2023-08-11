85c5c9da747c47df7ae51116f96fc04b7f2561fb
Indranil Bain <83275443+not-indro@users.noreply.github.com>
2023-08-12 01:02:57 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import altair as alt
 import sqlite3
 
 # Initialize SQLite database
@@ -12,15 +11,15 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS transactions
 conn.commit()
 
 # Streamlit app
+
+
 def main():
-    st.set_page_config(page_title="Modern Expense Tracker", page_icon="💰")  # Adding page icon
-    
+    st.set_page_config(page_title="Modern Expense Tracker", page_icon="💰")
     st.title("Modern Expense Tracker")
-    
-    # Sidebar
-    st.sidebar.header("Options")
-    selected_option = st.sidebar.radio("Select Option", ["Add Transaction", "View Transactions", "Expense Histogram"])
-    
+
+    options = ["Add Transaction", "View Transactions", "Expense Histogram"]
+    selected_option = st.sidebar.radio("Select Option", options)
+
     if selected_option == "Add Transaction":
         add_transaction()
     elif selected_option == "View Transactions":
@@ -29,60 +28,74 @@ def main():
         show_histogram()
 
 # Function to add a transaction
+
+
 def add_transaction():
     st.header("Add Transaction")
     date = st.text_input("Date", "")
     description = st.text_input("Description", "")
     amount = st.number_input("Amount", 0.0)
-    
+
     if st.button("Add"):
         if date and description and amount:
-            cursor.execute("INSERT INTO transactions (date, description, amount) VALUES (?, ?, ?)", (date, description, amount))
+            cursor.execute(
+                "INSERT INTO transactions (date, description, amount) VALUES (?, ?, ?)", (date, description, amount))
             conn.commit()
             st.success("Transaction added successfully.")
         else:
             st.error("Please fill in all fields.")
 
 # Function to view transactions
+
+
 def view_transactions():
     st.header("View Transactions")
-    
+
     cursor.execute("SELECT * FROM transactions")
     transactions = cursor.fetchall()
-    
+
     if transactions:
-        df = pd.DataFrame(transactions, columns=["ID", "Date", "Description", "Amount"])
+        df = pd.DataFrame(transactions, columns=[
+                          "ID", "Date", "Description", "Amount"])
         st.dataframe(df)
-        
+
         # Delete button for each transaction
         for index, row in df.iterrows():
             if st.button(f"Delete {row['ID']}"):
-                cursor.execute("DELETE FROM transactions WHERE id=?", (row['ID'],))
+                cursor.execute(
+                    "DELETE FROM transactions WHERE id=?", (row['ID'],))
                 conn.commit()
                 st.success("Transaction deleted successfully.")
-        
+
     else:
         st.warning("No transactions found.")
 
 # Function to show expense histogram
+
+
 def show_histogram():
     st.header("Expense Histogram")
-    
+
     cursor.execute("SELECT amount FROM transactions")
     amounts = cursor.fetchall()
-    
+
     if amounts:
         amounts = [amount[0] for amount in amounts]
-        
-        # Modernizing the histogram
-        plt.figure(figsize=(10, 6))
-        sns.histplot(data=amounts, bins=20, kde=True, color='skyblue')  # Added color and modified 'data' parameter
-        plt.xlabel("Amount")
-        plt.ylabel("Frequency")
-        plt.title("Expense Distribution")
-        st.pyplot(plt)
+
+        # Create Altair chart
+        chart = alt.Chart(pd.DataFrame({"Amount": amounts})).mark_bar().encode(
+            alt.X("Amount:Q", bin=alt.Bin(maxbins=20)),
+            y="count()"
+        ).properties(
+            title="Expense Distribution",
+            width=500,
+            height=300
+        )
+
+        st.altair_chart(chart)
     else:
         st.warning("No transactions found.")
+
 
 if __name__ == "__main__":
     main()
